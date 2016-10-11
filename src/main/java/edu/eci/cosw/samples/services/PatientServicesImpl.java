db5481aefbe4916ee41e12b482675cdc9b67831a
MstrJL300 <joslum3@icloud.com>
2016-10-10 22:54:32 -0500

    No esta aceptando la inyección del repositorio

---
+++ src/main/java/edu/eci/cosw/samples/services/PatientServicesImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cosw.samples.services;
+
+import edu.eci.cosw.jpa.sample.model.Paciente;
+import edu.eci.cosw.jpa.sample.repository.PatientsRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 2087133
+ */
+@Service
+public class PatientServicesImpl implements PatientServices{
+    
+    PatientsRepository patientsRepository;
+        
+    @Override
+    public Paciente getPatient(int id, String tipoid) throws ServicesException {        
+        return patientsRepository.findByPacienteID(id, tipoid);
+    }
+
+    @Override
+    public List<Paciente> topPatients(int n) throws ServicesException {
+        return patientsRepository.findByNConsulta(n);
+    }
+
+//    @Override
+//    public Paciente getPatient(int id, String tipoid) throws ServicesException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public List<Paciente> topPatients(int n) throws ServicesException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+    
+}
